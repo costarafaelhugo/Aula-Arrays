05c8b3000d6fcbcd17f8d610939df1f144ba81c1
Hugo Costa <costarafaelhugo@gmail.com>
2021-04-05 08:14:08 -0300

    test commit

--- src/main/kotlin/arrayMethods.kt
+++ src/main/kotlin/arrayMethods.kt
@@ -1,3 +1,11 @@
 fun main() {
+    val salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5.000, 8000.0, 10.000)
+
+    val aumento = 1.1
+
+    for (salario in salarios) {
+        salario * aumento
+    }
+
 
 }
